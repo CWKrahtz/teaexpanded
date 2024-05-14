d6774b0cd13c3b3b8b995aa556591743737154aa
Christian Krahtz <221198@virtualwindow.co.za>
2024-05-14 08:50:25 +0200

    Login and Register Screens Created
    Made the password hide the password (need to add functionality to toddle visibility).

---
+++ app/src/main/java/co/za/openwindow/tea_expanded/Screens/SignupScreen.kt
@@ -0,0 +1,145 @@
+package co.za.openwindow.tea_expanded.Screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import co.za.openwindow.tea_expanded.R
+import co.za.openwindow.tea_expanded.ui.theme.DarkBlue
+import co.za.openwindow.tea_expanded.ui.theme.TeaexpandedTheme
+import co.za.openwindow.tea_expanded.ui.theme.White
+
+@Composable
+fun SignupScreen(name: String, modifier: Modifier = Modifier) {
+    Column (
+        Modifier
+            .background(White)
+            .fillMaxSize()
+            .padding(
+                horizontal = 24.dp,
+                vertical = 50.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.tea),
+            contentDescription = "Logo",
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .width(75.dp)
+                .shadow(
+                    elevation = 15.dp,
+                    shape = RoundedCornerShape(CornerSize(50.dp)),
+                )
+                .padding(12.dp)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = "Create Account",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Row (
+            Modifier.fillMaxWidth(),
+        ){
+            Text(
+                text = "Create a new user account or "
+            )
+            Text(
+                text = "Login here",
+                textDecoration = TextDecoration.Underline,
+                color = DarkBlue
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+        var username by remember { mutableStateOf("Username") }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+        var email by remember { mutableStateOf("Email") }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+        var password by remember { mutableStateOf("Password") }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text  = "Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            trailingIcon = { Icon(painter = painterResource(id = R.drawable.eye), contentDescription = "Eye") },
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+        Column (
+            Modifier.fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+
+        ){
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF179CDE))
+            ) {
+                Text(
+                    text = "Create Account",
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignupScreenPreview() {
+    TeaexpandedTheme {
+        SignupScreen("Android")
+    }
+}
--- app/src/main/java/co/za/openwindow/tea_expanded/ui/theme/Color.kt
+++ app/src/main/java/co/za/openwindow/tea_expanded/ui/theme/Color.kt
@@ -10,6 +10,14 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+//Tutorial
 val Black = Color(0xFF000113)
-val LightBlueWhite = Color(0xFFF1F5F9)
+val LightBlueWhite = Color(0xFFF1F5F9) //Social Media Background
 val BlueGray = Color(0xFF334155)
+
+//Project
+val White = Color(0xFFFFFFFF)
+val DarkGrey = Color(0xFF333333)
+val LightGrey = Color(0xFF8C8C8C)
+val DarkBlue = Color(0xFF179CDE)
+val LightBlue = Color(0xFF0088CC)
